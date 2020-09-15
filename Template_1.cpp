0794dbb5b7a4524cf96558649e9834330ed2be9f
Priyansh19077 <55681439+Priyansh19077@users.noreply.github.com>
2020-09-15 11:43:40 +0530

    master

--- Template_1.cpp
+++ Template_1.cpp
@@ -15,6 +15,7 @@
 using namespace std;
 using namespace __gnu_pbds;
 using namespace chrono;
+#define debug(x) cout << #x << " " << x <<endl;
 #define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define MOD 1000000007
 #define MOD1 998244353
@@ -24,12 +25,11 @@ using namespace chrono;
 #define ff first
 #define ss second
 #define PI 3.141592653589793238462
-#define debug(x) cout << #x << " " << x <<endl;
 #define set_bits __builtin_popcount
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update > pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 /*---------------------------------------------------------------------------------------------------------------------------*/
 ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
 ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
@@ -41,7 +41,7 @@ void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
 ll combination(ll n, ll r, ll m, ll* fact) {ll val1 = fact[n]; ll val2 = mminvprime(fact[r], m); ll val3 = mminvprime(fact[n - r], m); return ((val1 * val2) % m * val3) % m;}
 void google(int t) {cout << "Case #" << t << ": ";}
 vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
-/*--------------------------------------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------------------------------------*/
 void solve()
 {
 
@@ -59,7 +59,7 @@ int main()
 	auto stop1 = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop1 - start1);
 #ifndef ONLINE_JUDGE
-	cerr << "Time: " << duration.count() / 1000.0 << endl;
+	cerr << "Time: " << duration.count() / 1000 << endl;
 #endif
 	return 0;
 }
