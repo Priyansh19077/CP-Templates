77f51d53f4958aa2e8c5f4164cebb2c1e6987ca0
Priyansh19077 <55681439+Priyansh19077@users.noreply.github.com>
2021-04-22 11:13:24 +0530

    Added Binary Lifting

---
+++ Binary_Lifting.cpp
@@ -0,0 +1,67 @@
+struct BinaryLifting {
+	int n;
+	int maxLog;
+	ll maxRequirement;
+	vector<vector<int>> parent;
+	vector<int> logValues;
+	bool precomputedLogs = false;
+	BinaryLifting(int n1, vector<int> *edges, ll requirement, int root) {
+		n = n1;
+		parent.resize(n);
+		maxLog = log2(requirement + 1);
+		maxRequirement = requirement;
+		for (int i = 0; i < n; i++) {
+			parent[i].resize(maxLog + 1);
+			for (int j = 0; j <= maxLog; j++) {
+				parent[i][j] = -1;
+			}
+		}
+		fillParentTable(root, edges);
+		if (maxRequirement <= 1000000LL)
+			precomputeLogs();
+	}
+	void fillParentTable(int root, vector<int> *edges) {
+		vector<bool> visited(n);
+		dfsBinaryLifting(root, edges, visited);
+		int intermediate = -1;
+		for (int i = 1; i <= maxLog; i++) {
+			for (int j = 0; j < n; j++) {
+				intermediate = parent[j][i - 1];
+				if (intermediate != -1) {
+					parent[j][i] = parent[intermediate][i - 1];
+				}
+			}
+		}
+	}
+	void dfsBinaryLifting(int root, vector<int> *edges, vector<bool> &visited) {
+		visited[root] = true;
+		for (auto i : edges[root]) {
+			if (!visited[i]) {
+				parent[i][0] = root;
+				dfsBinaryLifting(i, edges, visited);
+			}
+		}
+	}
+	void precomputeLogs() {
+		precomputedLogs = true;
+		logValues.resize(maxRequirement + 1);
+		logValues[1] = 0;
+		for (int i = 2; i <= maxRequirement; i++) {
+			logValues[i] = logValues[i / 2] + 1;
+		}
+	}
+	int kthParent(int start, int k) {
+		int a = start;
+		while (k > 0) {
+			int x = getLog(k);
+			a = parent[a][x];
+			if (a == -1)
+				return a;
+			k -= (1 << x);
+		}
+		return a;
+	}
+	inline int getLog(ll x) {
+		return precomputedLogs ? logValues[x] : log2(x);
+	}
+};
