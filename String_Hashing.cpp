9cd6d7b45b4dc98ce1694269439de9ea957de48c
Priyansh Agarwal <priyansh19077@iiitd.ac.in>
2022-02-08 21:39:55 +0530

    Added O(1) substring comparison

--- String_Hashing.cpp
+++ String_Hashing.cpp
@@ -27,7 +27,7 @@ struct Hashing{
             }
         }
     }
-    vector<ll> substringHash(int l, int r){
+    vector<ll> substringHash(int l, int r){ // extra O(log) factor
         vector<ll> hash(primes);
         for(int i = 0; i < primes; i++){
             ll val1 = hashValues[i][r];
@@ -35,5 +35,18 @@ struct Hashing{
             hash[i] = mod_mul(mod_sub(val1, val2, hashPrimes[i]), mminvprime(powersOfBase[i][l], hashPrimes[i]), hashPrimes[i]);
         }
         return hash;
+    }
+    bool compareSubstrings(int l1, int r1, int l2, int r2){ // use this for comparing strings faster
+        if(l1 > l2){
+            swap(l1, l2);
+            swap(r1, r2);
+        }
+        for(int i = 0; i < primes; i++){
+            ll val1 = mod_sub(hashValues[i][r1], (l1 > 0 ? hashValues[i][l1 - 1] : 0LL), hashPrimes[i]);
+            ll val2 = mod_sub(hashValues[i][r2], (l2 > 0 ? hashValues[i][l2 - 1] : 0LL), hashPrimes[i]);
+            if(mod_mul(val1, powersOfBase[i][l2 - l1], hashPrimes[i]) != val2)
+                return false;
+        }   
+        return true;
     }
 };
